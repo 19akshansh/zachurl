c4fe87de62eeba77bfe16bfa9d27561ca87f51a8
Akshansh Srivastava <akshanshsri.edu@gmail.com>
2026-06-16 13:54:18 +0000

    Added Not Found Page and removed useless logout Button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { checkAuth } from "@/lib/authUtils";
 import { getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { LogoutButton } from "@/features/auth/components/logoutBtn";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/appSidebar";
 import { AppHeader } from "@/components/appHeader";
@@ -26,7 +25,6 @@ const Page = async () => {
           <div className="p-8 flex flex-col gap-4">
             <div className="flex justify-between items-center">
               <h1 className="text-xl font-bold">Hi, {auth?.user?.name}</h1>
-              <LogoutButton />
             </div>
             <p>Welcome to your dashboard.</p>
           </div>
