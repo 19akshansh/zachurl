a181b83d99dbc805cf04e26f5433805778e54c2e
Akshansh Srivastava <akshanshsri.edu@gmail.com>
2026-06-16 13:56:15 +0000

    minor error fix

--- src/features/management/qr/components/qr.tsx
+++ src/features/management/qr/components/qr.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-export const QRForm = async () => {
+export const QRForm = () => {
   return (
     <div className="p-4 md:px-10 md:py-6 h-full">
       <div className="mx-auto mx-w-screen-md w-full flex flex-col gap-y-8 h-full">
